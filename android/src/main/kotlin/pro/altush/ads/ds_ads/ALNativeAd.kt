3a13db094adc54713b93c6138d8d070f3255149d
Alexey Tushurashvili <altush@yandex.ru>
2023-05-22 19:12:16 +0300

    fix native ad loading for some AppLovin adapters

--- android/src/main/kotlin/pro/altush/ads/ds_ads/ALNativeAd.kt
+++ android/src/main/kotlin/pro/altush/ads/ds_ads/ALNativeAd.kt
@@ -29,7 +29,6 @@ class ALNativeAd(
     fun load(context: Context) {
         val factory = manager.alFactories[factoryId]
             ?: throw Exception("Can't find NativeAdFactory with id: $factoryId")
-        nativeAdView = factory.createNativeAd()
 
         nativeAdLoader = MaxNativeAdLoader(adUnitId, context)
         nativeAdLoader!!.setRevenueListener(object : MaxAdRevenueListener {
@@ -45,7 +44,9 @@ class ALNativeAd(
         nativeAdLoader!!.setNativeAdListener(object : MaxNativeAdListener() {
             override fun onNativeAdLoaded(view: MaxNativeAdView?, ad: MaxAd) {
                 nativeAd = ad
+                nativeAdView = factory.createNativeAd()
                 manager.invokeMethod("onAdLoaded", mapOf("adId" to id))
+                nativeAdLoader!!.render(nativeAdView!!, ad)
             }
 
             override fun onNativeAdLoadFailed(adUnitId: String, error: MaxError) {
@@ -62,7 +63,7 @@ class ALNativeAd(
             }
         })
 
-        nativeAdLoader!!.loadAd(nativeAdView)
+        nativeAdLoader!!.loadAd()
     }
 
     fun dispose() {
